d40402fd88dd1c984a9cf91033c9148c4d6dd595
toniyide <57442950+toniyide@users.noreply.github.com>
2024-08-28 03:54:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
   images: {
     domains: ["eutech.cdn.prismic.io"],
   },
-};
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      // Add other pages here
+    }
+  },
+}
